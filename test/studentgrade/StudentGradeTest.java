813950a4b596b6b411ddc395a35af98375214dbc
KHALED2000m <164821339+KHALED2000m@users.noreply.github.com>
2024-04-05 22:11:44 +0300

    Update StudentGradeTest.java
    change test release

--- test/studentgrade/StudentGradeTest.java
+++ test/studentgrade/StudentGradeTest.java
@@ -1,33 +1,54 @@
 package studentgrade;
-import junit.framework.TestCase;
-/* JUnit 3.8.1 */
-public class StudentGradeTest extends TestCase {
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class StudentGradeTest {
     
-    public StudentGradeTest(String testName) {
-        super(testName);
-    }
-
-    public void testGetGrade95() {
-        System.out.println("getGrade 95");
-        int mark = 95;
-        String expResult = "A";
-        String result = StudentGrade.getGrade(mark);
-        assertEquals(expResult, result);
-    }
-
-    public void testGetGrade85() {
-        System.out.println("getGrade 85");
-        int mark = 85;
-        String expResult = "B";
-        String result = StudentGrade.getGrade(mark);
-        assertEquals(expResult, result);
+    public StudentGradeTest() {
     }
     
-    public void testGetGrade75() {
-        System.out.println("getGrade 85");
-        int mark = 75;
-        String expResult = "F";
+    @BeforeAll
+    public static void setUpClass() {
+    }
+    
+    @AfterAll
+    public static void tearDownClass() {
+    }
+    
+    @BeforeEach
+    public void setUp() {
+    }
+    
+    @AfterEach
+    public void tearDown() {
+    }
+
+    @Test
+    public void testMain() {
+        System.out.println("main");
+        String[] args = null;
+        StudentGrade.main(args);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getGrade method, of class StudentGrade.
+     */
+    @Test
+    public void testGetGrade() {
+        System.out.println("getGrade");
+        int mark = 0;
+        String expResult = "";
         String result = StudentGrade.getGrade(mark);
         assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
     }
+    
 }
